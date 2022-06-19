66d0f1a3baebcd6a1600ebbedc9377e8dac15745
cx33 <lne128gene980@163.com>
2022-06-19 17:41:05 +0800

    feat: add bug demo replicate

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
-import Layout from '@/layout/index.vue'
+import Layout from "@/layout/index.vue";
+
+function foo() {
+	console.log("test");
+}
+
+onMounted(() => {
+	foo();
+});
 </script>
 
 <template>
-  <Layout />
+	<Layout />
 </template>
 
 <style lang="scss" scoped>
+h3 {
+  @apply hover:bg-red-400;
+}
 </style>
